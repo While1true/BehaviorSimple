7389762634b8aef724d9aabb4583a13c208db0a4
While1true <893825883@qq.com>
2017-08-11 20:04:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ck/nest/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.ck.nest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView= (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(new RecyclerView.Adapter() {
+            @Override
+            public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                TextView textView = new TextView(parent.getContext());
+                textView.setText("dededsxxxxxxxxxxxxx");
+                textView.setHeight(200);
+                return new RecyclerView.ViewHolder(textView) {
+                    @Override
+                    public String toString() {
+                        return super.toString();
+                    }
+                };
+            }
+
+            @Override
+            public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+            }
+
+            @Override
+            public int getItemCount() {
+                return 20;
+            }
+        });
+    }
+}
